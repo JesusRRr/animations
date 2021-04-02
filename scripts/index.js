1a5b406d0444a6d66672bb66af2ebdb4d4fad345
Your Name <rolonjesus1996@gmail.com>
2021-04-02 14:14:43 -0600

    chore: drawing a circle

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 const canvas = document.getElementById('canvas2');
 const ctx = canvas.getContext('2d');
 console.log(ctx);
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+ctx.fillStyle = 'white';
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, Math.PI*2);
+ctx.fill();
