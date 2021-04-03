9976a58ccfb2775f031f1abb8aae6a65412c41eb
Your Name <rolonjesus1996@gmail.com>
2021-04-02 20:45:59 -0600

    chore: Particle class created

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,42 @@
 const canvas = document.getElementById('canvas2');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
-ctx.fillStyle = 'white';
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI*2);
-ctx.fill();
+
+const mouse = {
+    x: null,
+    y: null,
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+
+class Particle {
+    constructor(){
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 5;
+        this.speedX = Math.random * 3 - 1.5;
+        this.speedY = Math.random * 3 - 1.5;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x , this.y, this.size, 0, Math.PI*2);
+        ctx.fill();
+    }
+
+}
+
